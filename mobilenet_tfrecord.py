a1079c6eea99eeb0ba2756fde02f79bc30f0ca93
13181340 <kyh131230@gmail.com>
2019-02-10 14:11:55 +0900

    iterator for문 안에 배치

--- mobilenet_tfrecord.py
+++ mobilenet_tfrecord.py
@@ -58,9 +58,9 @@ def model(data, training=False):
     avg_pool = tf.nn.avg_pool(relu, ksize=[1, 7, 7, 1], strides=[1, 1, 1, 1], padding="VALID")
     reshape = tf.reshape(avg_pool, [-1, 1024])
 
-    pool = tf.nn.dropout(reshape, keep_prob)
+    #pool = tf.nn.dropout(reshape, keep_prob)
 
-    logits = tf.nn.relu(tf.matmul(pool, FC_w))
+    logits = tf.nn.relu(tf.matmul(reshape, FC_w))
     y_pred = tf.nn.softmax(logits)
 
     return y_pred, logits
@@ -194,87 +194,100 @@ conv14_b = tf.Variable(tf.constant(0.1, shape=[1024]))
 FC_w = tf.Variable(tf.truncated_normal([1024, class_num], stddev=0.1, dtype=tf.float32))
 
 
+
+
 train = tf.data.TFRecordDataset(filenames='./mobilenet_train.tfrecords', compression_type='GZIP')
 train = train.map(lambda record: parse_single_example(record))
-train = train.shuffle(buffer_size=10000)
-# train = train.repeat()
+train = train.shuffle(buffer_size=1000 + 3 * 128)
+train = train.repeat()
 train = train.batch(batch_size=128)
 
 
 test = tf.data.TFRecordDataset(filenames='./mobilenet_test.tfrecords', compression_type='GZIP')
 test = test.map(lambda record: parse_single_example(record))
 test = test.repeat()
-test = test.shuffle(buffer_size=int((75*0.4+3*128)))
+test = test.shuffle(buffer_size=1000 + 3 * 128)
 test = test.batch(batch_size=128)
 
 train_iterator = train.make_initializable_iterator()
-test_iterator = train.make_initializable_iterator()
+test_iterator = test.make_initializable_iterator()
 
 get_next_train = train_iterator.get_next()
 get_next_test = test_iterator.get_next()
 
-with tf.Session() as sess:
-    sess.run([train_iterator.initializer, test_iterator.initializer])
-    # X_data = sess.run(X)
-    # y_data = sess.run(y) 이렇게 따로 sess를 하게 되면 iterator가 각 자 돌아 총 데이터 개수인 75개에서 x_data : 50, y_label: 25개로 되는
-    # 오류가 생기게 된다.
-    x_train_data, y_train_labels = sess.run(get_next_train)
-    # print(len(y_train_labels))
-    x_test_data, y_test_labels = sess.run(get_next_test)
+
+
+
+# with tf.Session() as sess:
+    # sess.run([train_iterator.initializer, test_iterator.initializer])
+    # # X_data = sess.run(X)
+    # # y_data = sess.run(y) 이렇게 따로 sess를 하게 되면 iterator가 각 자 돌아 총 데이터 개수인 75개에서 x_data : 50, y_label: 25개로 되는
+    # # 오류가 생기게 된다.
+    # x_train_data, y_train_labels = sess.run(get_next_train)
+    # # print(len(y_train_labels))
+    # x_test_data, y_test_labels = sess.run(get_next_test)
+
 
 # print(">>>>>", x_train_data.shape)
 
 # print(x_data.shape, x_data.dtype) # (50, 224, 224, 3) float32
 # print(y_data.shape, y_data.dtype) # (50, ) int64
 
-    x = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
-    y = tf.placeholder(tf.int64, shape=[None, class_num])
-    keep_prob = tf.placeholder(tf.float32)
+x = tf.placeholder(tf.float32, shape=[None, 224, 224, 3])
+y = tf.placeholder(tf.int64, shape=[None, class_num])
+keep_prob = tf.placeholder(tf.float32)
 
-    y_train_one_hot = tf.one_hot(y_train_labels, class_num)
-    y_test_one_hot = tf.one_hot(y_test_labels, class_num)
+y_pred, logits = model(x, training=False)
 
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=y_pred))
+train_step = tf.train.AdamOptimizer(1e-3).minimize(loss)
 
-    y_pred, logits = model(x, training=False)
-
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=y_pred))
-    train_step = tf.train.AdamOptimizer(1e-3).minimize(loss)
-
-    correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1)) # [ True, False , .... 반환]
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) # 정확도 계산
+correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y, 1)) # [ True, False , .... 반환]
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) # 정확도 계산
 
 
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    sess.run([train_iterator.initializer, test_iterator.initializer])
+
+
+    # print(">>>>", y_train_one_hot)
+
+    # Epoch
+    # dataset_size = len(x_train_data)
+    # batch_size = 100
+
+    for i in range(10000):
+        x_train_data, y_train_labels = sess.run(get_next_train)
+        x_test_data, y_test_labels = sess.run(get_next_test)
+
+        y_train_one_hot = tf.one_hot(y_train_labels, class_num)
+        y_test_one_hot = tf.one_hot(y_test_labels, class_num)
+
         y_train_one_hot = y_train_one_hot.eval()
         y_test_one_hot = y_test_one_hot.eval()
-        # print(">>>>", y_train_one_hot)
 
-        # Epoch
-        # dataset_size = len(x_train_data)
-        # batch_size = 100
+        #print(">>>>>>>>", y_train_one_hot)
+        # Shuffle
+        #x_train_data, y_train_one_hot = data_shuffle(x_train_data, y_train_one_hot)
+        # Batch (min-batch size : 10)
+        # total_count = dataset_size // batch_size
+        # total_count = (total_count+1 if total_count > math.floor(total_count) else total_count)
+        #
+        # for batch_index in range(total_count):
+        #     batch = next_batch(batch_index, batch_size, x_train_data, y_train_one_hot)
+        #     sess.run(train_step, feed_dict={x: batch[0], y: batch[1], keep_prob: 0.8})
+        if i % 10 == 0:
+            train_accuracy = accuracy.eval(feed_dict={x: x_train_data, y: y_train_one_hot})#, keep_prob: 1.0})
 
-        for i in range(10000):
-            # Shuffle
-            #x_train_data, y_train_one_hot = data_shuffle(x_train_data, y_train_one_hot)
-            # Batch (min-batch size : 10)
-            # total_count = dataset_size // batch_size
-            # total_count = (total_count+1 if total_count > math.floor(total_count) else total_count)
-            #
-            # for batch_index in range(total_count):
-            #     batch = next_batch(batch_index, batch_size, x_train_data, y_train_one_hot)
-            #     sess.run(train_step, feed_dict={x: batch[0], y: batch[1], keep_prob: 0.8})
-
-            if i % 10 == 0:
-                train_accuracy = accuracy.eval(feed_dict={x: x_train_data, y: y_train_one_hot, keep_prob: 1.0})
-                y_p = y_pred.eval(feed_dict={x: x_train_data, y: y_train_one_hot, keep_prob: 1.0})
-                l = loss.eval(feed_dict={x: x_train_data, y: y_train_one_hot, keep_prob: 1.0})
-                print("Epoch: %d, 트레이닝 정확도 %f, Loss: %f"%(i, train_accuracy, l))
-            sess.run(train_step, feed_dict={x: x_train_data, y: y_train_one_hot, keep_prob: 0.8})
-        test_accuracy = 0.0
-        for i in range(10):
-            x_test_data, y_test_one_hot = data_shuffle(x_test_data, y_test_one_hot)
-            test_batch = test_pick(10, x_test_data, y_test_one_hot)
-            test_accuracy = test_accuracy + accuracy.eval(feed_dict={x: test_batch[0], y: test_batch[1], keep_prob: 1.0})
-        test_accuracy = test_accuracy / 10
-        print("테스트 데이터 정확도: %f" %test_accuracy)
+            y_p = y_pred.eval(feed_dict={x: x_train_data, y: y_train_one_hot})#, keep_prob: 1.0})
+            l = loss.eval(feed_dict={x: x_train_data, y: y_train_one_hot})#, keep_prob: 1.0})
+            print("Epoch: %d, 트레이닝 정확도 %f, Loss: %f"%(i, train_accuracy, l))
+        sess.run(train_step, feed_dict={x: x_train_data, y: y_train_one_hot})#, keep_prob: 0.8})
+    test_accuracy = 0.0
+    for i in range(10):
+        x_test_data, y_test_one_hot = data_shuffle(x_test_data, y_test_one_hot)
+        test_batch = test_pick(10, x_test_data, y_test_one_hot)
+        test_accuracy = test_accuracy + accuracy.eval(feed_dict={x: test_batch[0], y: test_batch[1], keep_prob: 1.0})
+    test_accuracy = test_accuracy / 10
+    print("테스트 데이터 정확도: %f" %test_accuracy)
